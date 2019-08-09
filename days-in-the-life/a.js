1c4f6350377e990e4b37eaacce6d9d992bfe960e
Itay Maman <imaman@users.noreply.github.com>
2019-08-09 18:17:52 +0300

    Revert "try to do grid"
    This reverts commit 8e4bfd7ce9eb30d990383d32415ba4d74bc5a921.

--- days-in-the-life/a.js
+++ days-in-the-life/a.js
@@ -155,7 +155,7 @@ const arrs = locations.map(l => readOneDay(`zips/${l.loc}`, l.dir === 'before'))
 
 function renderOneDay(a) {
     return a.map(curr => 
-        `<section><div class="my-grid" class="stretch"><div>${curr.timestamp}</div><div><img style="width: 100%; height: 100%" data-src="days-in-the-life/${curr.image}"></div></div></section>`).join('\n')
+        `<section><div class="col">${curr.timestamp}</div><div class="col"><img class="stretch" data-src="days-in-the-life/${curr.image}"></div></section>`).join('\n')
 }
 
 const replacement = arrs.map(curr => `<section>${renderOneDay(curr)}</section>`).join('\n')
